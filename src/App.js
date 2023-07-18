4e5f9a9c9c0e5079d65bfbda47d6465c3241a95d
tirthpatel2710 <tirthkumar@anbsystems.com>
2023-07-18 12:34:57 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>New Blog Website</h1>
     </div>
   );
---
+++ src/navbar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return ( 
+    <div className="navbar">
+        <p>Add New Blog</p>
+        <p>Current Blogs</p>
+    </div> 
+);
+}
+ 
+export default NavBar;
